59fed796164f6f1d81e429a0559cffd12dc433c4
Hebert Esteves <hebertesteves14.sp@gmail.com>
2026-04-20 19:34:23 -0300

    feat: cria entidade Frases

---
+++ ScreenmatchFrases/src/main/java/hebertesteves/ScreenmatchFrases/entity/Frases.java
@@ -0,0 +1,64 @@
+package hebertesteves.ScreenmatchFrases.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Frases {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String frase;
+    private String personagem;
+    private String titulo;
+    private String poster;
+
+    public Frases() {
+    }
+
+    public Frases(String frase, String personagem, String titulo, String poster) {
+        this.frase = frase;
+        this.personagem = personagem;
+        this.titulo = titulo;
+        this.poster = poster;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
+
+    public String getPersonagem() {
+        return personagem;
+    }
+
+    public void setPersonagem(String personagem) {
+        this.personagem = personagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+}
